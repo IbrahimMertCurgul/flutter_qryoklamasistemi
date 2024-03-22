25ce9ead8d3717e51bab0a949605c8bb61d95f09
IbrahimMertCurgul <imertc1@hotmail.com>
2024-03-22 16:27:46 +0300

    login1.0

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,2 +1,26 @@
-//login page
-//login screen
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+        body: Center(
+      child: Column(children: [
+        Container(
+          height: height,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/topkapilogo.png"))),
+        )
+      ]),
+    ));
+  }
+}
