5325bcd0de1bf6f2f854727e97e71eda95537c2b
IbrahimMertCurgul <imertc1@hotmail.com>
2024-03-29 02:40:36 +0300

    Klavyede Page Scroll Özelliği

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -22,212 +22,215 @@ class _LoginPageState extends State<LoginPage> {
     width = size.width;
     return Scaffold(
       extendBodyBehindAppBar: true,
-      resizeToAvoidBottomInset: false, //Ekran dışına taşma hatasını silme
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (width < 700 &&
-                width / height < 0.8) //MOBİLDE İF İÇİNDEKİ KODLAR GEÇERLİ
-              Column(
-                children: [
-                  Container(
-                    height: height * .45,
-                    decoration: const BoxDecoration(
-                        image: DecorationImage(
-                            fit: BoxFit.fitHeight,
-                            image: AssetImage("assets/images/arkaplan.png"))),
-                    child: Column(
-                      children: [
-                        AppBar(
-                          backgroundColor: Colors.transparent,
-                          elevation: 0,
-                          leading: const BackButton(color: Colors.white),
-                        ),
-                        Container(
-                          height: height * .25,
-                          decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.fitHeight,
-                                  image: AssetImage(
-                                      "assets/images/topkapilogo.png"))),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        RichText(
-                          text: const TextSpan(
-                            style:
-                                TextStyle(fontSize: 24.0, color: Colors.black),
-                            children: <TextSpan>[
-                              TextSpan(
-                                text: 'Merhaba!',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 32.0),
-                              ),
-                              TextSpan(
-                                text: '\nHoşgeldin',
-                              ),
-                            ],
-                          ),
-                        ),
-                        ////////////////////////////ÖĞRENCİ INPUT/////////////////////////////////
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
-                          child: TextField(
-                            obscureText: false,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
-                              ),
-                              labelText: 'Öğrenci Numarası',
-                            ),
-                          ),
-                        ),
-                        ////////////////////////////ŞİFRE INPUT/////////////////////////////////
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
-                          child: TextField(
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(30.0),
-                              ),
-                              labelText: 'Şifre',
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(40.0),
-                    child: SizedBox(
-                      height: 40.0,
-                      width: 150.0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                        ),
-                        child: ElevatedButton(
-                          onPressed: _signInWithEmailAndPassword,
-                          child: const Text(
-                            'Giriş Yap',
-                            style: TextStyle(fontSize: 20, color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            else //BİLGİSAYARDA ELSE İÇİNDEKİ KODLAR GEÇERLİ -------------------------
-              Container(
-                height: height,
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                  Color.fromARGB(255, 138, 35, 50),
-                  Color.fromARGB(255, 78, 4, 19),
-                ])),
-                child: Column(
+      resizeToAvoidBottomInset: true, //Ekran dışına taşma hatasını silme
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              if (width < 700 &&
+                  width / height < 0.8) //MOBİLDE İF İÇİNDEKİ KODLAR GEÇERLİ
+                Column(
                   children: [
                     Container(
-                      height: height * .25,
-                      margin: const EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
+                      height: height * .45,
                       decoration: const BoxDecoration(
                           image: DecorationImage(
                               fit: BoxFit.fitHeight,
-                              image:
-                                  AssetImage("assets/images/topkapilogo.png"))),
-                    ),
-                    Center(
+                              image: AssetImage("assets/images/arkaplan.png"))),
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-                          const Padding(
-                            padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                            child: Text(
-                              "Merhaba, Hoşgeldin!",
+                          AppBar(
+                            backgroundColor: Colors.transparent,
+                            elevation: 0,
+                            leading: const BackButton(color: Colors.white),
+                          ),
+                          Container(
+                            height: height * .25,
+                            decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.fitHeight,
+                                    image: AssetImage(
+                                        "assets/images/topkapilogo.png"))),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          RichText(
+                            text: const TextSpan(
                               style: TextStyle(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
+                                  fontSize: 24.0, color: Colors.black),
+                              children: <TextSpan>[
+                                TextSpan(
+                                  text: 'Merhaba!',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 32.0),
+                                ),
+                                TextSpan(
+                                  text: '\nHoşgeldin',
+                                ),
+                              ],
                             ),
                           ),
                           ////////////////////////////ÖĞRENCİ INPUT/////////////////////////////////
                           Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
-                            child: SizedBox(
-                              width: 500,
-                              child: TextField(
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30.0),
-                                  ),
-                                  labelText: 'Öğrenci Numarası',
+                            padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+                            child: TextField(
+                              obscureText: false,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
                                 ),
+                                labelText: 'Öğrenci Numarası',
                               ),
                             ),
                           ),
                           ////////////////////////////ŞİFRE INPUT/////////////////////////////////
                           Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-                            child: SizedBox(
-                              width: 500,
-                              child: TextField(
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  filled: true,
-                                  fillColor:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                  border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30.0),
-                                  ),
-                                  labelText: 'Şifre',
+                            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0),
                                 ),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(40.0),
-                            child: SizedBox(
-                              height: 50.0,
-                              width: 200.0,
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(30),
-                                  color:
-                                      const Color.fromARGB(255, 255, 255, 255),
-                                ),
-                                child: ElevatedButton(
-                                  onPressed: _signInWithEmailAndPassword,
-                                  child: const Text(
-                                    'Giriş Yap',
-                                    style: TextStyle(
-                                        fontSize: 20, color: Colors.black),
-                                  ),
-                                ),
+                                labelText: 'Şifre',
                               ),
                             ),
                           ),
                         ],
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: SizedBox(
+                        height: 40.0,
+                        width: 150.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            color: const Color.fromARGB(255, 255, 255, 255),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: _signInWithEmailAndPassword,
+                            child: const Text(
+                              'Giriş Yap',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
-                ),
-              )
-          ],
+                )
+              else //BİLGİSAYARDA ELSE İÇİNDEKİ KODLAR GEÇERLİ -------------------------
+                Container(
+                  height: height,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                    Color.fromARGB(255, 138, 35, 50),
+                    Color.fromARGB(255, 78, 4, 19),
+                  ])),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: height * .25,
+                        margin: const EdgeInsets.fromLTRB(0.0, 80.0, 0.0, 0.0),
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                fit: BoxFit.fitHeight,
+                                image: AssetImage(
+                                    "assets/images/topkapilogo.png"))),
+                      ),
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                              child: Text(
+                                "Merhaba, Hoşgeldin!",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            ////////////////////////////ÖĞRENCİ INPUT/////////////////////////////////
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
+                              child: SizedBox(
+                                width: 500,
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    labelText: 'Öğrenci Numarası',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            ////////////////////////////ŞİFRE INPUT/////////////////////////////////
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                              child: SizedBox(
+                                width: 500,
+                                child: TextField(
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    filled: true,
+                                    fillColor: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    labelText: 'Şifre',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(40.0),
+                              child: SizedBox(
+                                height: 50.0,
+                                width: 200.0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(30),
+                                    color: const Color.fromARGB(
+                                        255, 255, 255, 255),
+                                  ),
+                                  child: ElevatedButton(
+                                    onPressed: _signInWithEmailAndPassword,
+                                    child: const Text(
+                                      'Giriş Yap',
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.black),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
