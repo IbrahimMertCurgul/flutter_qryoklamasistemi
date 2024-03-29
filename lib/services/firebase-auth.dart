5f8899a30f1bccb08eb02882cdeeb25d72cb90f7
IbrahimMertCurgul <imertc1@hotmail.com>
2024-03-30 01:39:45 +0300

    Runtime Bugfix

--- lib/services/firebase-auth.dart
+++ lib/services/firebase-auth.dart
@@ -1,22 +1,22 @@
 // ignore_for_file: file_names
 
-import "package:cloud_firestore/cloud_firestore.dart";
+// import "package:cloud_firestore/cloud_firestore.dart";
 
-class AuthService {
-  final userCollection = FirebaseFirestore.instance.collection("users");
+// class AuthService {
+//   final userCollection = FirebaseFirestore.instance.collection("users");
 
-  Future<void> kullaniciKaydet(
-      {required String name,
-      required int surname,
-      required int email,
-      required int studentID,
-      required String password}) async {
-    await userCollection.doc().set({
-      "name": name,
-      "surname": surname,
-      "email": email,
-      "StudentID": studentID,
-      "Password": password,
-    });
-  }
-}
+//   Future<void> kullaniciKaydet(
+//       {required String name,
+//       required int surname,
+//       required int email,
+//       required int studentID,
+//       required String password}) async {
+//     await userCollection.doc().set({
+//       "name": name,
+//       "surname": surname,
+//       "email": email,
+//       "StudentID": studentID,
+//       "Password": password,
+//     });
+//   }
+// }
