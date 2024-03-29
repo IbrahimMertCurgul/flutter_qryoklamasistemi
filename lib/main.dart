48f6a1bed48ee0abedeea9f5b4fc47454562cbf0
IbrahimMertCurgul <imertc1@hotmail.com>
2024-03-29 16:27:59 +0300

    FireBase Entegrasyonu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/studentlogin.dart';
 import 'pages/teacherlogin.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const MaterialApp(home: MyApp()),
   );
--- lib/pages/teacherlogin.dart
+++ lib/pages/teacherlogin.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_typing_uninitialized_variables
-import 'dart:ui';
 import 'package:flutter/material.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
 
